b61de3cfb270b7d00689196955c796e4f6ff2bb3
Gabriel Jony Borges Silva <gjbsilva@intra.pgms.salvador.ba.gov.br>
2016-04-12 08:10:23 -0300

    add app Android Tutorial SQLite

---
+++ app/src/main/java/first/sqlite/android/vogella/de/exercicio7_tutorialsqlite/Comment.java
@@ -0,0 +1,32 @@
+package first.sqlite.android.vogella.de.exercicio7_tutorialsqlite;
+
+/**
+ * Created by GABRIEL on 07/04/2016.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
+
